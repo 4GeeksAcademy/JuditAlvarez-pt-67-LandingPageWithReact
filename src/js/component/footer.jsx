6fd2818546ab78f90ab548fb2d9b5f9734e7cc94
juditsoria <160534450+juditsoria@users.noreply.github.com>
2024-05-20 20:08:19 +0000

    casi

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react" ;
+const Footer =() => {
+    return (
+        <div>
+            <p> Copiright. Your website 2024</p>
+        </div>
+        
+    );
+};
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+import Footer from "./footer";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className=" container-fluid text-center">
+			
+			<div class ="bg-dark "><Navbar/></div>
+			<div class="container">
+			<div class= "bg-secondary bg-opacity-25"><Jumbotron/></div>
+			<div class= "row d-flex text-center justify-content-center m-3 p-3">
+				
+			  <Card/>
+			  <Card/>
+			  <Card/>
+			  <Card/>
+			</div>
+			</div>
+			<div class = " container-fluid bg-dark text-light"><Footer/></div>
+		
+	    </div>
+			
+	
 	);
 };
 
